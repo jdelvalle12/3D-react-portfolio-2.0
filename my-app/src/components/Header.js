0cce66d0a7650cc0ae7302af4c6dbcade2ecdec5
jdelvalle12 <104731082+jdelvalle12@users.noreply.github.com>
2023-04-09 11:25:35 -0400

    changed index.css & deleted codes in App.css

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link className="text-light" to="/">
-          <h1 className="m-0">Del Valle Digital Designs</h1>
-        </Link>
-      </div>
-    </header>
+    <header>
+    <div id="container">  
+        <a href="index.html" class="logo">Del Valle Digital Designs</a>   
+      <nav class="text-right">
+        <ul>
+          <li><a href="/about me">About Me</a></li> 
+          <li><a href="/projects">Projects</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
   );
 };
 
