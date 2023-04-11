9b27a35dec2dedf6e9a3247131ba65d1d531740c
Jose Del Valle <jdelvalle88@live.com>
2023-04-10 22:10:58 -0400

    small change in footer js file and header css

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import './Navbar.css';
+
 
 const Footer = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
+  
   return (
     <footer className="footer-container">
       <div className="container text-center mb-5">
-        {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            onClick={() => navigate(-1)}
-          >
-            &larr; Go Back
-          </button>
-        )}
-        <div className='copyright' >
+        
+          
+            
+         
+        <div className='copyright'>
           Made by © 2022 Del Valle Digital Designs
         </div>
       </div>
