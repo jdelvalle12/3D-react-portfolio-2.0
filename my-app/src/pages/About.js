07b43ea766d7a97f0a481f070febe598a6075455
jdelvalle12 <104731082+jdelvalle12@users.noreply.github.com>
2023-04-11 11:33:03 -0400

    edits to the about me content

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -12,12 +12,12 @@ export default function About() {
         <h1 className="aboutme">Hello my name is Jose</h1>
         <div />
         <div className='description'>
-          I am a full stack developer based in Orlando with experience in HTML5, CSS, variety of CSS framworks,
-          JavaScript, jQuery, and have used Web, Third party & Server-side APIs on the front end 
-          development. In addition, on the back end development, experience in MySQL, Node.js, 
-          Express.js, REACT, GraphQL & MongoDB. Having an appetite for continuous learning, collaboration, 
-          and problem-solving. I plan to create and maintain websites while also developing apps. Aside from 
-          programming, I like listen to a variety of music, videogaming and travel around the world. 
+          I am a full stack developer based in Orlando with experience in HTML5, CSS, variety of CSS frameworks,
+          JavaScript, jQuery, and used Web, Third party & Server-side APIs on the front end 
+          development. In addition, on the back end development, I have experience in MySQL, Node.js, 
+          Express.js, REACT, GraphQL & MongoDB. I have an appetite for continuous learning, collaboration, 
+          and problem-solving, and I plan to create and maintain websites while also developing apps. Aside from 
+          programming, I like listen to a variety of music, play videogames and travel around the world. 
         </div>
       </div>
     </section>
