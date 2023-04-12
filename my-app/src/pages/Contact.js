fa5797a7b992b5a1c4784fcd2ea0d6a49669a015
jdelvalle12 <104731082+jdelvalle12@users.noreply.github.com>
2023-04-12 17:22:47 -0400

    just about finished the contact page

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -3,30 +3,27 @@ import '../styles/Contact.css';
 
 export default function Contact() {
   return (
+  
     <div className='contact-container'>
-      <h1>Contact Page</h1>
-      <div class="design-inquiries">
-      <h3>Design Inquiries:</h3>
-      <form>
-        <ul>
-          <li>
-            <label class="text-right" for="name">Name:</label>
-            <input type="text" id="name" name="user-name"/>
-          </li>
-          <li>
-            <label class="text-right" for="email">Email:</label>
-            <input type="text" id="email" name="user-email"/>
-          </li>
-          <li>
-            <label class="text-right" for="message">Message:</label>
-            <input type="text" id="message" name="user-message"/>
-          </li>
-        </ul>
-        <div class="text-center">
-          <button type="submit">SEND</button>
-        </div>
-      </form>
+      <h1 className='contact-title'>Contact</h1>
+      <div className="design-inquiries">
+        <h3 className='contact-header'>Design Inquiries:</h3>
+        <form>
+          <label className="text-right" htmlFor="name">Name:</label>
+          <input type="text" id="name" name="user-name"/>
+          <br />
+          <label className="text-right" htmlFor="email">Email:</label>
+          <input type="text" id="email" name="user-email"/>
+          <br />
+          <label className="text-right" htmlFor="message">Message:</label>
+          <input type="text" id="message" name="user-message"/>
+          <br />
+          <div className="text-center">
+            <button type="submit">SEND</button>
+          </div>
+        </form>
+      </div>
     </div>
-    </div>
+ 
   );
 }
