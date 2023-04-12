883cc123abe7595b600d527b7da0032ec58d3ebc
jdelvalle12 <104731082+jdelvalle12@users.noreply.github.com>
2023-04-12 01:56:37 -0400

    small adjustment to projects page

--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -19,28 +19,28 @@ const Projects = () => {
               <div className="project-card-overlay">
                 <h3><a href="https://flick-tracker.herokuapp.com/">Flick-Tracker</a></h3>
               </div>
-                <p>Description of Flick-Tracker</p>
+                
               </div>
               <div className="project-card">
                 <img src={manifesto} alt="manifesto" />
                   <div className="project-card-overlay">
                   <h3><a href="https://stickkman.github.io/gamblingManifesto-Proj01/">Manifesto</a></h3>
               </div>
-                <p>Description of Manifesto</p>
+                
               </div>
               <div className="project-card">
                 <img src={SEO} alt="SEO" />
               <div className="project-card-overlay">
                   <h3><a href="https://jdelvalle12.github.io/horiseon-webpage/">SEO</a></h3>
               </div>
-                <p>Description of SEO</p>
+                
               </div>
               <div className="project-card">
                 <img src={Journeez} alt="Journeez" />
               <div className="project-card-overlay">
                   <h3><a href="https://example.com/journeez">Journeez</a></h3>
               </div>
-                <p>Description of Journeez</p>
+                
               </div>
             </div>
           </div>
